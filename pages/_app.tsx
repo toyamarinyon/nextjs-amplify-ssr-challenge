b2ae4b949d9a29aa70ef775222fa0f2ab4f7652f
toyamarinyon <toyamarinyon@gmail.com>
2022-07-04 13:40:11 +0900

    add authenticator

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import awsconfig from '../src/aws-exports';
+import { Amplify, Auth } from 'aws-amplify';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+Amplify.configure(awsconfig);
 
 export default MyApp
